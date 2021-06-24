cdbd3622b81ea84376ee7af98781e9868e8870ce
Wen Jen <wjen1214@gmail.com>
2021-06-24 10:45:49 -0700

    add single product page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Ibay</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Ibay</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/sign-in'>
-                <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
