299855e0a05f710658df427b6b8138ed98490797
Wen Jen <wjen1214@gmail.com>
2021-07-02 17:57:38 -0700

    set up server error handler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import products from './data/products.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -14,7 +15,13 @@ app.get('/', (req, res) => {
   res.send(`API is running...`);
 });
 
-app.use('/api/products', productRoutes);
+app.use('/api/v1/products', productRoutes);
+
+// Catch routes not found
+app.use(notFound);
+
+// Error Handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
